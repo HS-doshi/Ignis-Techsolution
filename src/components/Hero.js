5a98d2d7f1cf20467f1476d1c67f5b78c2f7c181
Heet Doshi <doshiheet2872003@gmail.com>
2023-05-03 11:16:48 +0530

    Initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="../images/photo-grid.png" className="hero--photo" alt="Hero img" />
+            <h1 className="hero--header">Ignis Tech Solutions</h1>
+            <p className="hero--text">Dynamic Page using React JS! All about learning...
+                Using props, create components from array!! I enjoyed a lot from this frontend assignment of Ignis Techsolutions. </p>
+        </section>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="/images/airbnb-logo.png" className="nav--logo" alt="Airbnb Logo" />
+
+            {/* <img src={Image1} alt="Airbnb img" className="nav--logo" /> */}
+        </nav>
+    )
+}
